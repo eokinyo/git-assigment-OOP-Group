039c87ac59647d6d8b840976e08455a130173e06
eokinyo <50834185+eokinyo@users.noreply.github.com>
2023-11-21 10:08:26 +0200

    Methods created

--- src/phoneApp.java
+++ src/phoneApp.java
@@ -5,8 +5,42 @@ public class phoneApp {
 	}
 
 }
-Class Phone{
+class Phone{
 	public String brand;
 	public String model;
-	public int baterryLevel;
+	public int batteryLevel;
+	
+
+	public Phone(String brand, String model, int batteryLevel)
+	{
+		this.brand = brand;
+		this.model = model;
+		this.batteryLevel = batteryLevel;
+		printData();
+		
+	}
+	
+	
+	//Creating the public methods: charge, use and printData
+		public void charge() //Creating the charge method
+		{
+			System.out.println("Phone is charging");
+		}
+		public void use() //Creating the use method
+		{
+			if (batteryLevel > 0)
+			{
+			System.out.println("Phone is in use");
+			batteryLevel--;
+			}
+		}
+		public void printData() //Creating the printData method
+		{
+			System.out.println("Brand: " + brand);
+			System.out.println("Model: " + model);
+			System.out.println("Battery level: " + batteryLevel);
+		}
 }
+
+
+	
