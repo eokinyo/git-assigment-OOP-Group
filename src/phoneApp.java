9e400d06014a1322dc63ab17c11efa71d5178036
eokinyo <50834185+eokinyo@users.noreply.github.com>
2023-11-21 09:47:44 +0200

    Group members added

--- src/phoneApp.java
+++ src/phoneApp.java
@@ -4,6 +4,8 @@ public class phoneApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Group Members");
+		System.out.println("Kosova Nart, AN Nguyen Anh, AN Novacsek Adam, Okinyo Elly Okinyo Elly");
 	}
 
 }
